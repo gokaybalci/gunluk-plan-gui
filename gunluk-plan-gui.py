48df6a51f71ea3262e7fa36c74e9ea52c709183f
gokaybalci <gokaybalci@outlook.com>
2023-05-06 16:15:03 +0300

    GUI düzeltmeleri

--- gunluk-plan-gui.py
+++ gunluk-plan-gui.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 import requests
 import docx
+import tkinter.ttk as ttk
 
 from bs4 import BeautifulSoup
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 from docx import Document
 
 
@@ -30,7 +31,6 @@ def download_daily_plan():
 
     for link in eslesen_haftalar:
         if ('.docx' in link.get('href', [])):
-            status_label.config(text="Dosya indiriliyor...")
             # Get response object for link
             response = requests.get(link.get('href'))
 
@@ -54,44 +54,46 @@ def download_daily_plan():
 
     document.save(kademe + ". Sınıf " + hafta + ". Hafta" + ".docx")
 
-    status_label.config(text="Dosya güncellendi.")
+    status_label.config(text="Dosyanız hazır.")
 
 root = tk.Tk()
 root.title("Gunluk Plan GUI")
+root.resizable(False, False)
 
 # Set the window size
-root.geometry("400x400")
+root.geometry("350x375")
 
-
-my_font = ('Century Gothic', 13)
+# font setup
+input_font = ('Century Gothic', 13)
+button_font = ('Century Gothic', 17)
 
 # create input widgets for user inputs
-kademe_label = tk.Label(root, text="Kademe giriniz:", font=my_font)
+kademe_label = tk.Label(root, text="Kademe giriniz:", font=input_font)
 kademe_label.pack(side="top", padx=3, pady=3)
 kademe_entry = tk.Entry(root)
 kademe_entry.pack(side="top", padx=1, pady=1)
 
-hafta_label = tk.Label(root, text="Günlük plan haftası:", font=my_font)
+hafta_label = tk.Label(root, text="Günlük plan haftası:", font=input_font)
 hafta_label.pack(side="top", padx=3, pady=3)
 hafta_entry = tk.Entry(root)
 hafta_entry.pack(side="top", padx=1, pady=1)
 
-oisim_label = tk.Label(root, text="Öğretmen ismi:", font=my_font)
+oisim_label = tk.Label(root, text="Öğretmen ismi:", font=input_font)
 oisim_label.pack(side="top", padx=3, pady=3)
 oisim_entry = tk.Entry(root)
 oisim_entry.pack(side="top", padx=1, pady=1)
 
-misim_label = tk.Label(root, text="İdareci ismi:", font=my_font)
+misim_label = tk.Label(root, text="İdareci ismi:", font=input_font)
 misim_label.pack(side="top", padx=3, pady=3)
 misim_entry = tk.Entry(root)
 misim_entry.pack(side="top", padx=1, pady=1)
 
 # create a button to execute the code
-execute_button = tk.Button(root, text="İndir", command=download_daily_plan, font=my_font)
-execute_button.pack(side="top", padx=5, pady=5)
+execute_button = tk.Button(root, text="İndir", command=download_daily_plan, font=button_font)
+execute_button.pack(side="top", padx=5, pady=25)
 
 # Create a label to show the status of the process
-status_label = tk.Label(root, text="")
+status_label = tk.Label(root, text="", font=button_font)
 status_label.pack()
 
 root.mainloop()
